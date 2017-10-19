d1d16d62c79b6626016dae9382f80d396e83bdcf
bqlin <bqlins@163.com>
2017-10-19 16:09:40 +0800

    Initial Commit

---
+++ NSURLSessionDownloadExp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSURLSessionDownloadExp
+//
+//  Created by BqLin on 2017/10/19.
+//  Copyright © 2017年 POLYV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
